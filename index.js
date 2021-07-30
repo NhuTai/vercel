bd43a2f3b354b768159e367f953012e69ebe551e
TaiNguyen073 <75308712+TaiNguyen073@users.noreply.github.com>
2021-07-30 09:31:23 +0700

    add user

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
 const product = require("./api/product");
-
+var mysql = require('mysql');
+var router = express.Router();
+var con = mysql.createConnection({
+    host: "154.16.171.82",
+    user: "admin",
+    password: "HDy0c0QK",
+    port: 18831,
+    database: "snack"
+});
 app.use(express.json({ extended: false }));
 
 app.use("/api/product", product);
-
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM user", function (err, result, fields) {
+        if (err) throw err;
+        cus = result;
+    });
+});
+app.use('/users', function(req, res, next) {
+    res.send(cus);
+});
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
